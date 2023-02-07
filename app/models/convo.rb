e8f8c9fbf7f583983a4f84c3d6d064756bfff588
Tim <tim.andrejc02@gmail.com>
2023-02-07 09:54:32 +0100

    trying to make convos

--- app/models/convo.rb
+++ app/models/convo.rb
@@ -1,4 +1,7 @@
 class Convo < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  has_many :messages, dependent: :destroy
+  validates :post, presence: true
+  validates :user, presence: true
 end
