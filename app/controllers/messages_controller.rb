7e6a80c8aae2632896d0d4045cdf4eaefc7e4135
Tim <tim.andrejc02@gmail.com>
2023-02-10 18:59:28 +0100

    frontend looking good

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,24 @@
 class MessagesController < ApplicationController
-    before_action :authenticate_user!, except: %i[ index show ]
-
+  before_action :authenticate_user!, except: %i[ index show ]
+  before_action :message_empty, only: %i[ create ]
     def new
         @message = Message.new
     end
-
     def create
-        @message = Message.new(message_params)
-        @convo = Convo.find(params[:message][:convo_id])
-        @message.convo_id = @convo.id
-        @message.user_id = current_user.id
-        @message.message = params[:message][:message]
-        respond_to do |format|
+      @message = Message.new(message_params)
+      @convo = Convo.find(params[:message][:convo_id])
+      @message.convo_id = @convo.id
+      @message.user_id = current_user.id
+      @message.message = params[:message][:message]
+      respond_to do |format|
         if @message.save
-            @convo.updated_at = @message.created_at
-            @convo.save
-            format.html { redirect_to convo_url(@convo)}
-            format.json { render :show, status: :created, location: @convo}
-
+          @convo.updated_at = @message.created_at
+          @convo.save
+          format.html { redirect_to convo_url(@convo)}
+          format.json { render :show, status: :created, location: @convo}
         else
-            format.html { render :new, status: :unprocessable_entity }
-            format.json { render json: @convo.errors, status: :unprocessable_entity }
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @convo.errors, status: :unprocessable_entity }
         end
       end
     end
@@ -33,4 +31,10 @@ def authorize_user!
 end
 def message_params
   params.require(:message).permit( :message, :convo_id, :user_id)
+end
+def message_empty
+  if params[:message][:message].empty?
+    @convo = Convo.find(params[:message][:convo_id])
+    redirect_to convo_url(@convo)
+  end
 end
