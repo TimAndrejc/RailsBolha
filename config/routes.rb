4c417b01dbc3b1c5413faa2150c83fe8383bae0c
Tim <tim.andrejc02@gmail.com>
2023-02-18 14:32:46 +0100

    +looks nicer

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     resources :convos, only: [:create]
   end
   get '/rails/active_storage/disk/:encoded_key/*filename', to: 'active_storage/disk#show'
-  get 'myposts', to: 'posts#myposts', as: 'myposts'
+  get 'MyPosts', to: 'posts#myposts', as: 'myposts'
   get 'admin', to: 'posts#admin', as: 'admin'
 
   #get '*path', to: 'errors#not_found'
