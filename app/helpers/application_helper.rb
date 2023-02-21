6fb0209c332ef0906d41ecb2daa8725d304c0438
Tim <tim.andrejc02@gmail.com>
2023-02-21 12:57:19 +0100

    heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,4 @@
-module ApplicationHelper
-    def display_notice
-        content_tag(:div, flash[:notice], class: "notice") if flash[:notice]
-    end
-    def display_alert
-        content_tag(:div, flash[:alert], class: "alert") if flash[:alert]
-    end
-
+module ApplicationHelper 
 
 def display_navbar_options
     if user_signed_in?
@@ -14,24 +7,7 @@ def display_navbar_options
         render 'loggedOutNav'
     end
 end
-def on_file_autoloaded(file)
-    cref  = autoloads.delete(file)
-    cpath = cpath(*cref)
 
-    Zeitwerk::Registry.unregister_autoload(file)
-
-    if cdef?(*cref)
-      log("constant #{cpath} loaded from file #{file}") if logger
-      to_unload[cpath] = [file, cref] if reloading_enabled?
-      run_on_load_callbacks(cpath, cget(*cref), file) unless on_load_callbacks.empty?
-    else
-      msg = "expected file #{file} to define constant #{cpath}, but didn't"
-      log(msg) if logger
-      crem(*cref)
-      to_unload[cpath] = [file, cref] if reloading_enabled?
-      raise Zeitwerk::NameError.new(msg, cref.last)
-    end
-end
 
 def display_active_class(link_path)
     "active" if current_page?(link_path)
