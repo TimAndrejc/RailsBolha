73361b2ee451ba53340a9658f56d2970b0da6ef4
Tim <tim.andrejc02@gmail.com>
2023-02-21 13:04:16 +0100

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
-module ApplicationHelper 
+module ApplicationHelper
+    def display_notice
+        content_tag(:div, flash[:notice], class: "notice") if flash[:notice]
+    end
+    def display_alert
+        content_tag(:div, flash[:alert], class: "alert") if flash[:alert]
+    end
+
 
 def display_navbar_options
     if user_signed_in?
@@ -7,7 +14,6 @@ def display_navbar_options
         render 'loggedOutNav'
     end
 end
-
 
 def display_active_class(link_path)
     "active" if current_page?(link_path)
